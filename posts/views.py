aa96f46a12cd9af4454def84ba114cfb4f174a83
Santiago Martinez <peladio98@gmail.com>
2023-12-16 12:21:58 -0400

    first commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Posts
+
+# Create your views here.
+
+class HomePageView(ListView):
+    model= Posts
+    template_name= "home.html"
